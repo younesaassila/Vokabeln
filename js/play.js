c35f2bcfc378aff3155d2083ee3fa2621c4854ce
Yougi3 <younes.aassila@outlook.fr>
2020-06-04 19:59:38 +0200

    Refonte feedback.html + about.html et fin de l'implémentation du modal 'Signaler une erreur de traduction'

--- js/play.js
+++ js/play.js
@@ -2,9 +2,14 @@
 const parameters = new URLSearchParams(window.location.search);
 const id = parameters.get('id');
 
+// Reference the Firebase database used for feedback reports.
+const database = firebase.database();
+
 // Get all paths associated with the id we just obtained.
 const dataAccess = new DataAccess();
 const paths = dataAccess.getPaths(id);
+
+//#region HTML elements references.
 
 // HTML elements in the quiz section.
 const quizContainer = document.querySelector("#quiz-container");
@@ -13,10 +18,24 @@ const quizWord = document.querySelector(".quiz-word");
 const quizTextInput = document.querySelector("#quiz-text-input");
 const quizButton = document.querySelector("#quiz-button-input");
 const endQuizButton = document.querySelector("#quiz-end-button");
+// HTML elements for the feedback modal.
+const feedbackButton = document.querySelector("#feedback-button");
+const feedbackModal = document.querySelector("#feedback-modal");
+const fbCloseButton = document.querySelector("#feedback-modal #close-button");
+const fbOriginalTextInput = document.querySelector("#feedback-modal #input-original");
+const fbTranslationsTextInput = document.querySelector("#feedback-modal #input-translations");
+const fbSendButton = document.querySelector("#feedback-modal #send-button");
+const fbBodyInputs = document.querySelector("#feedback-modal #body-inputs");
+const fbBodySuccess = document.querySelector("#feedback-modal #body-success");
+const fbBodyError = document.querySelector("#feedback-modal #body-error");
+const fbOkayButton = document.querySelector("#feedback-modal #okay-button");
+const fbRetryButton = document.querySelector("#feedback-modal #retry-button");
 // HTML elements in the results section.
 const resultsContainer = document.querySelector("#results-container");
 const resultsScore = document.querySelector("#results-score");
 const resultsDetails = document.querySelector("#results-details");
+
+//#endregion
 
 // Get an array of questions from the newly gotten paths and create a new quiz.
 const questions = dataAccess.getQuestionsFromListPaths(paths);
@@ -104,8 +123,7 @@ const answerQuestion = () => {
 const wordCount = quiz.questions.length;
 document.title += ` (${wordCount} mots)`;
 
-// Add a click event to the main button.
-quizButton.addEventListener("click", (event) => {
+quizButton.addEventListener("click", event => {
   if (event.button === 0) {
     switch (state) {
       case states.ANSWERING:
@@ -118,10 +136,8 @@ quizButton.addEventListener("click", (event) => {
   }
 }, false);
 
-// Get keyboard input in order to click on the button when the player presses Enter.
-window.addEventListener("keydown", (event) => {
+quizTextInput.addEventListener("keydown", event => {
   if (event.defaultPrevented) {
-    // Do nothing if the event was already processed.
     return;
   }
   switch (event.key) {
@@ -129,14 +145,104 @@ window.addEventListener("keydown", (event) => {
       quizButton.click();
       break;
     default:
-      // Quit when this doesn't handle the key event.
       return;
   }
-  // Cancel the default action to avoid it being handled twice.
   event.preventDefault();
 }, true);
 
-endQuizButton.addEventListener("click", (event) => {
+// Display the feedback modal.
+const showFeedbackModal = () => {
+  if (state == states.ANSWERING) {
+    quizButton.click();
+  }
+  // Show the view with text fields.
+  fbBodyInputs.style.display = "block";
+  fbBodySuccess.style.display = "none";
+  fbBodyError.style.display = "none";
+  // Set text fields values.
+  fbOriginalTextInput.value = quiz.questions[quiz.index]
+    .getWord().toString().replace(/,/g, ', ');
+  fbTranslationsTextInput.value = quiz.questions[quiz.index]
+    .getCorrectAnswers().toString().replace(/,/g, ', ');
+  // Set the send button values.
+  fbSendButton.disabled = false;
+  fbSendButton.innerHTML = "Envoyer";
+  // Display the modal.
+  feedbackModal.style.display = "flex";
+  fbOriginalTextInput.focus();
+};
+feedbackButton.addEventListener("click", showFeedbackModal);
+feedbackButton.addEventListener("keydown", event => {
+  if (event.defaultPrevented) {
+    return;
+  }
+  switch (event.key) {
+    case "Enter":
+      showFeedbackModal();
+      break;
+    default:
+      return;
+  }
+  event.preventDefault();
+}, true);
+
+// Close the feedback modal when clicking on the close or okay button.
+const closeFeedbackModal = () => {
+  feedbackModal.style.display = "none";
+  quizButton.click();
+};
+fbCloseButton.addEventListener("click", closeFeedbackModal);
+fbOkayButton.addEventListener("click", closeFeedbackModal);
+
+// Check if one of the text fields is empty or whitespaces in order to disable
+// the send button.
+const feedbackInputUpdate = () => {
+  fbSendButton.disabled = 
+    (/^\s*$/).test(fbOriginalTextInput.value) || 
+    (/^\s*$/).test(fbTranslationsTextInput.value);
+};
+fbOriginalTextInput.addEventListener("input", feedbackInputUpdate);
+fbTranslationsTextInput.addEventListener("input", feedbackInputUpdate);
+
+// Send the feedback report to the database.
+fbSendButton.addEventListener("click", () => {
+  // Disable the send button.
+  fbSendButton.disabled = true;
+  fbSendButton.innerHTML = "Envoi...";
+
+  // Send request to the database.
+  var newKey = database.ref().child('posts').push().key;
+  database.ref(`/feedback-reports/${newKey}`).set({
+    datetime: new Date().toString(),
+    listID: id,
+    quizIndex: quiz.index,
+    original: quiz.questions[quiz.index].getWord().toString().replace(/,/g, ', '),
+    translations: quiz.questions[quiz.index].getCorrectAnswers().toString().replace(/,/g, ', '),
+    originalCorrected: fbOriginalTextInput.value,
+    translationsCorrected: fbTranslationsTextInput.value
+  })
+  .then(() => {
+    // Display the success message.
+    fbBodyInputs.style.display = "none";
+    fbBodySuccess.style.display = "block";
+  })
+  .catch(error => {
+    // Display the error message.
+    fbBodyInputs.style.display = "none";
+    fbBodyError.style.display = "block";
+    console.error(`${error.code}: ${error.message}`);
+  });
+});
+
+fbRetryButton.addEventListener("click", () => {
+  fbBodyError.style.display = "none";
+  fbSendButton.disabled = false;
+  fbSendButton.innerHTML = "Envoyer";
+  fbBodyInputs.style.display = "block";
+});
+
+// Display the total score at the end of the quiz.
+endQuizButton.addEventListener("click", event => {
   if (event.button === 0) {
     quizContainer.style.display = "none";
     const scoreOn20 = Math.floor((quiz.stats.correctCount / quiz.stats.answeredCount * 20) * 10) / 10;
@@ -145,6 +251,11 @@ endQuizButton.addEventListener("click", (event) => {
     resultsContainer.style.display = "block";
   }
 }, false);
+
+// Log in the user anonymously.
+firebase.auth().signInAnonymously().catch(error => {
+  console.error(`${error.code}: ${error.message}`);
+});
 
 // Start the game!
 endQuizButton.disabled = true;
